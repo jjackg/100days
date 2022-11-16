1efd2fe8ed0bb5845e1d793dde96c02c52463c5e
jjackg <j.j.gibbs95@gmail.com>
2022-11-17 00:05:05 +1100

    day 33 final commit

--- day33/intro.py
+++ day33/intro.py
@@ -1,12 +1,30 @@
 import requests
 
-response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+MY_LAT = -37.813629
+MY_LNG = 144.963058
+
+
+# response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude,latitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0,
+}
+    
+response = requests.get("https://api.sunrise-sunset.org/json", params= parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude,latitude)
-print(iss_position)
+print(sunrise.split("T"), sunset.split("T"))
